8d2b711dc5b21db36f7044293831251af3cabc3d
Miguel William <116318328+miguelwilliam@users.noreply.github.com>
2026-03-16 22:25:53 -0300

    user e jwt funcionando com uuid

--- api/models.py
+++ api/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+import uuid
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
     first_name = None
     last_name = None
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     email = models.EmailField(unique=True)
 
