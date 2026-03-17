f1ad447b88bdc5bf2be3da043079471f9ed94f49
Miguel William <116318328+miguelwilliam@users.noreply.github.com>
2026-03-17 10:45:30 -0300

    terminei os serializer base + viewsets com bugfix

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Movie, Room, Session, Reservation, Seat, SessionSeat
 
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Informações pessoais", {"fields": ("username",)}),
+        ("Permissões", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Datas importantes", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "password1", "password2"),
+        }),
+    )
+
+    list_display = ("email", "username", "is_staff")
+    search_fields = ("email", "username")
+    ordering = ("email",)
+
 # Register your models here.
-admin.site.register(User, UserAdmin)
 admin.site.register(Movie)
 admin.site.register(Room)
 admin.site.register(Session)
