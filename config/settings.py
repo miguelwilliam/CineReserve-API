0f0f18d49b11f3315b743655f36c363d2492e707
Miguel William <116318328+miguelwilliam@users.noreply.github.com>
2026-03-18 10:44:26 -0300

    quick update adicionando paginação pagenumber

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
