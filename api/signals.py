5bc036d8e4d423315f4fcbef49367e48f27d31a8
Miguel William <116318328+miguelwilliam@users.noreply.github.com>
2026-03-18 17:32:14 -0300

    signal para auto preencher session-seats criado

--- api/signals.py
+++ api/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from api.models import Seat
+from api.models import Seat, Session, SessionSeat, Room
 
 @receiver(post_save, sender=Seat)
 def update_total_seats_on_create(sender, instance, **kwargs):
@@ -14,3 +14,21 @@ def update_total_seats_on_delete(sender, instance, **kwargs):
     room = instance.room
     room.total_seats = room.seats.count()
     room.save()
+
+@receiver(post_save, sender=Session)
+def create_session_seats(sender, instance, created, **kwargs):
+    if not created:
+        return
+    
+    seats = instance.room.seats.all()
+
+    session_seats = [
+        SessionSeat(
+            session=instance,
+            seat=seat,
+            status=SessionSeat.SeatStatus.AVAILABLE
+        )
+        for seat in seats
+    ]
+
+    SessionSeat.objects.bulk_create(session_seats)
