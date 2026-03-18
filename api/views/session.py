b71fde00b3770bb73d1017fdbea88b33fdb2ebb5
Miguel William <116318328+miguelwilliam@users.noreply.github.com>
2026-03-18 11:52:46 -0300

    query ?movie adicionado para filtrar as sessões

--- api/views/session.py
+++ api/views/session.py
@@ -7,3 +7,12 @@ class SessionViewSet(ModelViewSet):
     permission_classes = [AllowAny]
     queryset = Session.objects.all()
     serializer_class = SessionSerializer
+
+    def get_queryset(self):
+        queryset = Session.objects.all().select_related('movie', 'room')
+        movie_id = self.request.query_params.get('movie')
+
+        if movie_id and movie_id.isdigit():
+            queryset = queryset.filter(movie_id=movie_id)
+
+        return queryset
